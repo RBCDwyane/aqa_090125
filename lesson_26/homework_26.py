b52f8950fcb2539b4b21791bf05188817e29ceef
RBCDwyane <113509087+RBCDwyane@users.noreply.github.com>
2025-05-13 17:42:08 +0300

    Update lesson_26/homework_26.py
    fuction adding

--- lesson_26/homework_26.py
+++ lesson_26/homework_26.py
@@ -7,28 +7,29 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-url = f"http://localhost:8000/dz.html"
-driver = webdriver.Chrome()
-driver.get(url)
+def frames_verifing(secret1, secret2):
+    url = "http://localhost:8000/dz.html"
+    driver = webdriver.Chrome()
+    driver.get(url)
 
-driver.switch_to.frame(driver.find_element(By.ID, "frame1"))
-input_field = driver.find_element(By.ID, "input1")
-input_field.send_keys("Frame1_Secret")
-check_button = driver.find_element(By.XPATH, '//button[@onclick="verifyInput(\'input1\')"]')
-check_button.click()
-alert = driver.switch_to.alert
-print(alert.text)
-alert.accept()
+    driver.switch_to.frame(driver.find_element(By.ID, "frame1"))
+    input_field = driver.find_element(By.ID, "input1")
+    input_field.send_keys(secret1)
+    check_button = driver.find_element(By.XPATH, '//button[@onclick="verifyInput(\'input1\')"]')
+    check_button.click()
+    alert = driver.switch_to.alert
+    print(alert.text)
+    alert.accept()
 
-driver.switch_to.default_content()
+    driver.switch_to.default_content()
 
-driver.switch_to.frame(driver.find_element(By.ID, "frame2"))
-input_field = driver.find_element(By.ID, "input2")
-input_field.send_keys("Frame2_Secret")
-check_button = driver.find_element(By.XPATH, '//button[@onclick="verifyInput(\'input2\')"]')
-check_button.click()
-alert = driver.switch_to.alert
-print(alert.text)
-alert.accept()
+    driver.switch_to.frame(driver.find_element(By.ID, "frame2"))
+    input_field = driver.find_element(By.ID, "input2")
+    input_field.send_keys(secret2)
+    check_button = driver.find_element(By.XPATH, '//button[@onclick="verifyInput(\'input2\')"]')
+    check_button.click()
+    alert = driver.switch_to.alert
+    print(alert.text)
+    alert.accept()
 
-driver.quit()
+    driver.quit()
